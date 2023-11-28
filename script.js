48e985db001165fe5743129e7e925ed1c18c0163
spicyramen21 <151119237+spicyramen21@users.noreply.github.com>
2023-11-27 20:12:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const noBtn = document.querySelector(".no-btn");
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "I cant wait to see you tomorrow";
   gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+    "https://media.giphy.com/media/Y4WDXbagwPoepikUdJ/giphy.gif";
 });
 
 noBtn.addEventListener("mouseover", () => {
